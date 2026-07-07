700df1befd81bbd7cc83244dd9617f8dff6196ac
Carl Broker <carlbroker@gmail.com>
2026-07-07 18:26:14 -0500

    Add YouTube icon and clearer hint text for expandable video cards

--- web_app.py
+++ web_app.py
@@ -222,7 +222,7 @@ def _results(results):
 <div class="result-title">{r["vt"]}</div>
 <div class="result-id mono small">{r["vid"]}</div>
 <div class="result-text">{text}</div>
-<div class="video-expand-hint">▶ CLICK TO WATCH</div>
+<div class="video-expand-hint"><svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor" style="vertical-align:middle;margin-right:4px"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg> CLICK TO WATCH ON YOUTUBE</div>
 <div class="video-embed"><div class="embed-frame"><iframe allow="autoplay; encrypted-media" allowfullscreen loading="lazy"></iframe></div></div>
 </div>'''
     h += '</div>'
